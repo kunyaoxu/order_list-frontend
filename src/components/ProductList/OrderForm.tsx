1376f0f6e836df129d1779a5e21135e8dd0cee68
kunyaoxu <29974104+kunyaoxu@users.noreply.github.com>
2019-05-15 20:23:14 +0800

    Create callPostOrdersApi action

--- src/components/ProductList/OrderForm.tsx
+++ src/components/ProductList/OrderForm.tsx
@@ -2,22 +2,74 @@ import React from 'react';
 import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 import { useMappedState, useDispatch } from 'redux-react-hook';
 import { IAppReduxState } from 'reducers';
+import { onInputChange, callPostOrdersApi } from 'reducers/orderform/actions';
 
+const onChangeWrap = (e: React.ChangeEvent<HTMLInputElement>) => {
+  return {
+    key: e.target.name,
+    value: e.target.value
+  };
+}
+
+const preventEnterSubmit = (e: React.KeyboardEvent<HTMLInputElement>) => {
+  e.charCode === 13 && e.preventDefault();
+}
 
 const OrderForm: React.FC = () => {
+  const formData = useMappedState((state: IAppReduxState) => state.orderform)
+  const dispatch = useDispatch();
+
   return (
-    <Form className="order_form">
+    <Form
+      className="order_form"
+      onSubmit={
+        (e) => {
+          e.preventDefault();
+          e.stopPropagation();
+          dispatch(callPostOrdersApi(formData));
+          return false;
+        }
+      }
+    >
       <FormGroup>
         <Label for="name">Name</Label>
-        <Input type="text" name="name" required={true}/>
+        <Input
+          type="text"
+          name="name"
+          required={true}
+          onChange={
+            (e) => {
+              dispatch(onInputChange(onChangeWrap(e)));
+            }
+          }
+        />
       </FormGroup>
       <FormGroup>
         <Label for="email">Email</Label>
-        <Input type="email" name="email" pattern="[\w-+.]+@[\w]+(\.[\w]+)+" required/>
+        <Input
+          type="email"
+          name="email"
+          pattern="[\w-]+@[\w]+(\.[\w]+)+"
+          required
+          onChange={
+            (e) => {
+              dispatch(onInputChange(onChangeWrap(e)));
+            }
+          }
+        />
       </FormGroup>
       <FormGroup>
         <Label for="address">Address</Label>
-        <Input type="text" name="address" required/>
+        <Input
+          type="text"
+          name="address"
+          required
+          onChange={
+            (e) => {
+              dispatch(onInputChange(onChangeWrap(e)));
+            }
+          }
+        />
       </FormGroup>
       <Button>Submit</Button>
     </Form>
--- src/reducers/orderform/actions.tsx
+++ src/reducers/orderform/actions.tsx
@@ -1,7 +1,11 @@
 import { apiUrl } from '../const';
+import { IOrderFormData } from './interface';
 
 const TOGGLE_PRODUCT_CHECKBOX = 'TOGGLE_PRODUCT_CHECKBOX';
 const ON_INPUT_CHANGE = 'ON_INPUT_CHANGE';
+const ON_PRODUCT_SELECT_LESS_THAN_ONE = 'ON_PRODUCT_SELECT_LESS_THAN_ONE';
+const API_POST_ORDERS_SUCCESS = 'API_POST_ORDERS_SUCCESS';
+const API_POST_ORDERS_FAIL = 'API_POST_ORDERS_FAIL';
 
 export const toggleCheckbox = (id: number) => {
   return {
@@ -21,9 +25,54 @@ export const onInputChange = (obj: {key: string, value: string}) => {
   };
 }
 
+export const postOrdersSuccess = () => {
+  return {
+    type: API_POST_ORDERS_SUCCESS,
+    payload: {
+    }
+  };
+}
+
+export const postOrdersFail = () => {
+  return {
+    type: API_POST_ORDERS_FAIL,
+    payload: {
+    }
+  };
+}
+
+export const callPostOrdersApi = (dataToSend: IOrderFormData) => {
+  if (dataToSend.product_ids.length === 0) {
+    return {
+      type: ON_PRODUCT_SELECT_LESS_THAN_ONE,
+      payload: {}
+    }
+  }
+  return (dispatch: any) => {
+    fetch(apiUrl + '/orders', {
+      method: "POST",
+      headers: {
+        "content-type": "application/json"
+      },
+      body: JSON.stringify(dataToSend)
+    })
+      .then(function(response) {
+        return response.json();
+      })
+      .then((data) => {
+        dispatch(postOrdersSuccess());
+      })
+      .catch((data) => {
+        dispatch(postOrdersFail());
+      });
+  };
+}
+
 export type ActionType =
   typeof TOGGLE_PRODUCT_CHECKBOX |
-  typeof ON_INPUT_CHANGE;
+  typeof ON_INPUT_CHANGE |
+  typeof ON_PRODUCT_SELECT_LESS_THAN_ONE |
+  typeof API_POST_ORDERS_SUCCESS;
 
 export type ActionPayloadType =
   ReturnType<typeof toggleCheckbox>['payload'] &
